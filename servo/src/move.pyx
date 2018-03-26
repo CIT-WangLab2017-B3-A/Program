2776eae561c666a5d972cf3bf17177a3d0f7247c
pi tobita <s1526122TM@s.chibakoudai.jp>
2018-03-26 19:14:02 +0900

    update: but no test by yumigeta

--- servo/src/move.pyx
+++ servo/src/move.pyx
@@ -6,76 +6,49 @@ import sys, os.path
 import serial
 import time
 from uart import *
-class movei(uart):
+class move(uart):
     def __init__(self, LEG_SERVOS=3, port='/dev/ttyS0', rate=115200):
         super().__init__(port, rate)
         self.LegServos = LEG_SERVOS
-        self.Torque(0xFF, self.servo.ON)
+        self.Torque(0xFF, self.ON)
     def FileOpen(self, FileName=None):
-        if FileName!=None:
-            if os.path.isfile(FileName):
-                self.fp = open(FileName,'r')
-                self.flag = True
-            else:
-                self.flag = False
-        else:
-            self.flag = False
+        try:
+            self.fp = open(FileName,'r')
+            self.it = iter(fp.readline,'')# イテレータ
+        except:
+            return IOError
     def FileClose(self):
-        if self.flag:
+        try:
             self.fp.close()
-            self.flag = False
     def DataImport(self):
-        self.Data=[]
-        if self.flag:
-            while True:
-                self.line = self.fp.readline()
-                if not self.line:
-                    self.flag = False
-                    return None
-                self.DataList = self.line[:-1].split(',')
-                self.Group = int(self.DataList[0])
-                self.Speed = self.DataList[1]
-                for i in xrange(self.LegServos):
-                    self.VID = (3*int(self.Group))+i+1
-                    self.Angle = self.DataList[i+2]
-                    #self.Speed
-                    
-                    self.TmpData=self.servo.Angle_Speed(self.Angle, self.Speed)
-                    self.Data.append([self.VID, self.TmpData[0], self.TmpData[1]])
-                if self.DataList[-1]!=('&' or '&'+'\r'):
-                    break
-            return self.Data
-            
-    def MakePacket(self, Data):
-        self.TxData = self.servo.LongPacket(0x1E, Data)
-        return self.TxData
-    def Stand(self, FileName):
-        self.FileOpen(FileName)
-        self.Data = self.DataImport()
-        self.servo.Write(self.MakePacket(self.Data))
-        time.sleep(3.0)
-        self.FileClose()
-    def ahead(self, FileName, sleep):
-        self.FileOpen('parameter/Ahead/'+FileName)
-        self.Data = self.DataImport()
-        while self.Data!=None:
-            self.servo.Write(self.MakePacket(self.Data))
-            time.sleep(sleep)
-            self.Data = self.DataImport()
-        self.FileClose()
-        
+        self.Data = []
+        while True:
+            try:
+                line = self.it.next()# 1行ずつ読み取る
+            except StopIteration:
+                return None
+            DataList = line[:-1].split(',')# 配列化
+            # DataListの処理
+            Group  = DataList[0]# Group
+            fSpeed = DataList[1]# Speed
+            for i in xrange(self.LegServos):
+                VID    = (3*int(Group)) + (i+1)# ID
+                fAngle = DataList[i+2]# 角度
+                # float to int
+                [iAngle, iSpeed] = self.Angle_Speed(fAngle, fSpeed)
+                self.Data.append([VID, iAngle, iSpeed])# 2d array
+            if DataList[-1] != ('&' or '&'+'\r'):# 配列の最後
+                break
+        self.Data.pop(0)
+        return self.Data
     def Action(self, FileName, sleep):
-        self.FileOpen('parameter/'+FileName)
-        self.Data = self.DataImport()
-        while self.Data!=None:
-            self.servo.Write(self.MakePacket(self.Data))
-            time.sleep(sleep)
+        try:
+            self.FileOpen('parameter/'+FileName)
             self.Data = self.DataImport()
-        self.FileClose()
-    def Start(self):
-        self.servo.Start()
-    def Stop(self):
-        self.servo.Stop()
-    def Close(self):
-        self.servo.Close()
-        
+            while self.Data != None:
+                self.Write(self.LongPacket(self.ADDRESS_POS, self.Data))
+                time.sleep(sleep)
+                self.Data = self.DataImport()
+            self.FileClose()
+        except IOError:
+            print 'File is not found'
--- servo/src/uart.pyx
+++ servo/src/uart.pyx
@@ -11,22 +11,28 @@ class uart(object):
         self.OFF    = 0x00
         self.ON     = 0x01
         self.PANTCH = 0x02
+        # address
+        self.ADDRESS_POS = 0x1E
         # open port
-        self.uart=serial.Serial(port, rate)
+        self.uart = serial.Serial(port, rate)
+    # Write to servo
     def Write(self, TxData):
         self.uart.write(TxData)
         time.sleep(0.06)
+    # float to int
     def Angle_Speed(self, fAngle, fSpeed):
-        Angle = int(10.0*float(fAngle))
+        Angle = int(10.0 *float(fAngle))
         Speed = int(100.0*float(fSpeed))
         tmpData = [Angle, (Angle>>8), Speed, (Speed>>8)]
         self.Data = map(lambda x:x&0x00FF, tmpData)
         return self.Data
+    # calculate CheckSum
     def CheckSum(self, Data):
         self.check=0x00
         for x in Data:
             self.check ^= x
         return self.check
+    # ShortPacket
     def ShortPacket(self, ID, Flag, Address, Cnt, Data):
         # packet header
         self.TxData = [0xFA, 0xAF]
@@ -40,7 +46,7 @@ class uart(object):
             Length = 0x01
             tmpData = [ID, Flag, Address, Length, Cnt]
             tmpData.append(Data)
-        # checkSum
+        # CheckSum
         tmpData.append(self.CheckSum(tmpData))
         self.TxData.extend(tmpData)
         return self.TxData
@@ -50,7 +56,7 @@ class uart(object):
         Length = len(Data[0])# data par servo
         Cnt = len(Data)# servos
         tmpData = [0x00, 0x00, Address, Length, Cnt]
-        for x in xrange(Data):
+        for x in Data:
             tmpData.extend(x)
         # checkSum
         tmpData.append(self.CheckSum(tmpData))
@@ -58,52 +64,54 @@ class uart(object):
         return self.TxData
     #control func
     def Reboot(self, ID):
-        self.TxData = self.ShortPacket(ID, 0x20, 0xFF, 0x00, None)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(ID, 0x20, 0xFF, 0x00, None)
+        self.Write(TxData)
+        print 'Reboot:Finish!'
     def RomWrite(self, ID):
-        self.TxData = self.ShortPacket(ID, 0x40, 0xFF, 0x00, None)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(ID, 0x40, 0xFF, 0x00, None)
+        self.Write(TxData)
+        print 'Write ROM:Finish!'
     def ChangeID(self, NewID):
-        self.TxData = self.ShortPacket(0xFF, 0x00, 0x04, 0x01, NewID)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(0xFF, 0x00, 0x04, 0x01, NewID)
+        self.Write(TxData)
         self.RomWrite(NewID)
         self.Reboot(NewID)
-        print 'Finish!'
+        print 'Change ID:Finish!'
     def Inverse(self, ID, SW):
-        self.TxData = self.ShortPacket(ID, 0x00, 0x05, 0x01, SW)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(ID, 0x00, 0x05, 0x01, SW)
+        self.Write(TxData)
         self.RomWrite(ID)
         self.Reboot(ID)
-        print 'Finish!'
+        print 'Inverse Rotate:Finish!'
     def Torque(self, ID, SW):
-        self.TxData = self.ShortPacket(ID, 0x00, 0x24, 0x01, SW)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(ID, 0x00, 0x24, 0x01, SW)
+        self.Write(TxData)
     def Start(self):
         self.Torque(0xFF, self.ON)
     def Stop(self):
         self.Torque(0xFF, self.PANTCH)
     def ZeroAll(self):
-        self.Data = self.Angle_Speed(0, 0.01)
+        Data = self.Angle_Speed(0, 0.01)
         self.Torque(0xFF, self.ON)
-        self.TxData = self.ShortPacket(0xFF, 0x00, 0x1E, 0x01, self.Data)
-        self.Write(self.TxData)
+        TxData = self.ShortPacket(0xFF, 0x00, 0x1E, 0x01, Data)
+        self.Write(TxData)
         time.sleep(2.0)
         self.Stop()
+    # testProgram
     def Tester(self,ID):
         self.Torque(ID, self.ON)
         for j in xrange(2):#two loop
-            self.Data = self.Angle_Speed(30, 0.01)
-            self.TxData = self.ShortPacket(ID, 0x00, 0x1E, 0x01, self.Data)
-            self.Write(self.TxData)
+            Data = self.Angle_Speed(30, 0.01)
+            TxData = self.ShortPacket(ID, 0x00, 0x1E, 0x01, Data)
+            self.Write(TxData)
             time.sleep(1.0)
-            self.Data = self.Angle_Speed(0, 0.01)
-            self.TxData = self.ShortPacket(ID, 0x00, 0x1E, 0x01, self.Data)
-            self.Write(self.TxData)
+            Data = self.Angle_Speed(0, 0.01)
+            TxData = self.ShortPacket(ID, 0x00, 0x1E, 0x01, Data)
+            self.Write(TxData)
             time.sleep(1.0)
         self.Torque(ID, self.OFF)
     def Close(self):
         self.Torque(0xFF,self.OFF)
         self.uart.close()
     def __del__(self):
-        self.Torque(0xFF,self.OFF)
-        self.uart.close()
+        self.Close()
