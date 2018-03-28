e59b9e8f89047b75826992dbfeb4127505960108
pi tobita <s1526122TM@s.chibakoudai.jp>
2018-03-28 15:20:49 +0900

    edit sample program

--- servo/main.py
+++ servo/main.py
@@ -1,59 +1,39 @@
 #!/usr/bin/python
 # coding: utf-8
 
-## model: Futaba RS304MD
-## Temprate of move program
+# sample program
 
 # import Library
 from move import move
 import time
+import numpy as np
 
 # main program
 def main():
+    # nomal array
+    Data=[\
+        [[0],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+        [[3],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+        [[4],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+    ]
+
+    # numpy array
+    npData=np.array([\
+        [[1],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+        [[2],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+        [[5],[20.0,0.1],[20.0,0.1],[20.0,0.1]],\
+    ])
+
+    # class move(LEG_SERVOS=3, port='/dev/ttyS0', rate=115200)
     servo = move()
-    servo.Action('BallCatch.csv',1.0)
-    
-    for i in xrange(5):
-    	'''
-    	for i in xrange(8):
-		temp_str = "walk08/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-	'''
-    	'''
-    	for i in xrange(12):
-		temp_str = "walk12/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-	'''
-    	'''
-    	for i in xrange(16):
-		temp_str = "walk16/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-        '''
-        '''
-    	for i in xrange(24):
-		temp_str = "walk24/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-        '''
-	'''
-    	for i in xrange(24):
-		temp_str = "walk24high/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-    	'''
-	'''
-    	for i in xrange(16):
-		temp_str = "turn_right/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-	'''
-	''''
-    	for i in xrange(16):
-		temp_str = "turn_left/test" + str(i) + ".csv"
-		servo.Action(temp_str,0.01)
-	'''
-    servo.Stop()
-#    servo.Action('Ball/BallCatch.csv',0.55)
-#    servo.Stop()
-    #time.sleep(1.0)
-#    servo.Action('Ball/BallDust.csv',0.55)
+
+    # 従来型(file)
+    servo.Action('zero.csv',1.0)
+    # 配列(普通の)
+    servo.Action(Data)
+    # 配列(numpy)
+    servo.Action(npData)
+
     time.sleep(1.0)
     servo.Close()
 
