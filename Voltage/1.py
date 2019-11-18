3b96c0507071d1eb143e5886c85e1b33c55d85cb
pi-tobita <s1526122TM@s.chibakoudai.jp>
2019-11-18 14:15:05 +0900

    edit Voltage monitor

--- Voltage/1.py
+++ Voltage/1.py
@@ -1,17 +1,51 @@
+#!/usr/bin/python
+#coding: utf-8
+import os
 import wiringpi as wp
 import time
 
 # SPI channle (0 or 1)
 MCP_CH = 0
 # pin base (above 64)
-PIN_BASE=65
+PIN_BASE=70
+
+# registance
+#R1=4.10*1000.0  # [Ω]
+#R2=5.32*1000.0  # [Ω]
+R1=3.9*1000.0  # [Ω]
+R2=5.6*1000.0  # [Ω]
+
+#voltage
+Vcc_MAX=12.0 # [V]
+Vdd=4.8 # [V]
+Vref=Vdd
+Vlsb=Vref/1024.0
+
+#amplitude
+#Iin= 10.0*0.000001 # [A]
+Iin= 0.000644972877057 # [A]
 
 # setup
 wp.mcp3002Setup (PIN_BASE, MCP_CH)
-#wp.wiringPiSetupGpio()
+wp.wiringPiSetupGpio()
+SumRegistance = R1+R2
 
+#sumI=0.0
+#for i in xrange(100):
 while True:
-    value = float(wp.analogRead(PIN_BASE))*0.004652981427175*2.421052631579
-#    value = float(wp.analogRead(PIN_BASE))*0.0048828125*2.4358974359
-    print (value)
-    time.sleep(1)
+    mcp3002_data = float(wp.analogRead(PIN_BASE+MCP_CH))
+    Vin = ((mcp3002_data/1024.0)*Vdd) # 2.988[V] if Vcc=7.4[V]
+    Vcc = (Vin/R2+Iin)*SumRegistance
+    if Vcc < 6.2:
+        print "<ここでシャットダウン操作>"
+        time.sleep(2.0)
+        break
+        #os.system('sudo poweroff')
+    elif Vcc < 6.3: # <3.15[V/cel](絶対目安)
+        print "ヤバイ(早く充電): ",
+    elif Vcc < 6.6: # 3.15<3.3[V/cel](安心目安)
+        print "ピンチ(そろそろ充電): ",
+    else:
+        print "大丈夫(使用可能): ",
+    print Vcc
+    time.sleep(1.0)
--- servo/main.py
+++ servo/main.py
@@ -13,13 +13,30 @@ def main():
     servo = move()
     servo.Stand('parameter/StandAll.csv')
     time.sleep(1.0)
-    servo.ahead('Ahead0.csv',0.01)
-    for i in xrange(10):
-        servo.ahead('DownRight.csv',0.05)
-        servo.ahead('PullRight.csv',0.075)
-        servo.ahead('DownLeft.csv',0.05)
-        servo.ahead('PullLeft.csv',0.075)
+    
+    for i in xrange(12):
+        print "###"
+        servo.Action('test/test1.csv', 0.01)
+        servo.Action('test/test2.csv', 0.01)
+        servo.Action('test/test3.csv', 0.01)
+        servo.Action('test/test4.csv', 0.01)
+        servo.Action('test/test5.csv', 0.01)
+        servo.Action('test/test6.csv', 0.01)
+        #servo.Action('test/test7.csv', 0.01)
+        #servo.Action('test/test8.csv', 0.01)
+        #servo.Action('test/test9.csv', 0.01)
+        #servo.Action('test/test10.csv', 0.01)
+        #servo.Action('test/test11.csv', 0.01)
     servo.Stand('parameter/StandAll.csv')
+    servo.ahead('Ahead0.csv',0.01)
+
+#    for i in xrange(10):
+#        servo.ahead('DownRight.csv',0.05)
+#        servo.ahead('PullRight.csv',0.075)
+#        servo.ahead('DownLeft.csv',0.05)
+#        servo.ahead('PullLeft.csv',0.075)
+#    servo.Stand('parameter/StandAll.csv')
+
 #    servo.Action('Ball/BallCatch.csv',0.55)
 #    servo.Stand('parameter/StandAll.csv')
 #    for i in xrange(5):
